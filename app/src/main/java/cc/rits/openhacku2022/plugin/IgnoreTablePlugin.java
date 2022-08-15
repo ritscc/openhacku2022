37e512d4f152072f18696778e38039be55d2369c
averak <abe12@mccc.jp>
2022-08-15 15:52:50 +0900

    [chore] flywayとmybatisをセットアップ

---
+++ app/src/main/java/cc/rits/openhacku2022/plugin/IgnoreTablePlugin.java
@@ -0,0 +1,52 @@
+package cc.rits.openhacku2022.plugin;
+
+import java.util.List;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+/**
+ * MyBatis Generatorで不要なテーブルを無視するプラグイン
+ */
+public class IgnoreTablePlugin extends PluginAdapter {
+
+    private final List<String> IGNORE_TABLES = List.of( //
+        "flyway_schema_history", //
+        "SPRING_SESSION", //
+        "SPRING_SESSION_ATTRIBUTES" //
+    );
+
+    @Override
+    public boolean validate(final List<String> warnings) {
+        return true;
+    }
+
+    private boolean checkIsTableToGenerate(final IntrospectedTable introspectedTable) {
+        return this.IGNORE_TABLES.stream() //
+            .noneMatch(introspectedTable.getFullyQualifiedTableNameAtRuntime()::equals);
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(final TopLevelClass topLevelClass, final IntrospectedTable introspectedTable) {
+        return this.checkIsTableToGenerate(introspectedTable);
+    }
+
+    @Override
+    public boolean modelExampleClassGenerated(final TopLevelClass topLevelClass, final IntrospectedTable introspectedTable) {
+        return this.checkIsTableToGenerate(introspectedTable);
+    }
+
+    @Override
+    public boolean clientGenerated(final Interface interfaze, final IntrospectedTable introspectedTable) {
+        return this.checkIsTableToGenerate(introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        return this.checkIsTableToGenerate(introspectedTable);
+    }
+
+}
