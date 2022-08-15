6fbc757de7f2cc0db8f21b63c3a9d1339fb74885
averak <abe12@mccc.jp>
2022-08-15 15:30:13 +0900

    [chore] Gradleプロジェクトを作成

---
+++ app/src/main/java/cc/rits/openhack2022/App.java
@@ -0,0 +1,11 @@
+package cc.rits.openhack2022;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
