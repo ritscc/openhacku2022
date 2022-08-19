ab7698a64adf2346f29a164ba32b48979d621a58
averak <abe12@mccc.jp>
2022-08-20 06:06:31 +0900

    [feat] メニューリスト取得APIを呼び出し

--- app/src/main/client/src/app/customer/component/order/menus/menus.component.ts
+++ app/src/main/client/src/app/customer/component/order/menus/menus.component.ts
@@ -1,82 +1,35 @@
 import { Component, OnInit } from "@angular/core";
 import { MenuResponse } from "@api/models/menu-response";
+import { MenuService } from "@api/services/menu.service";
+import { TransactionService } from "@api/services/transaction.service";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 
+@UntilDestroy()
 @Component({
     selector: "app-menus",
     templateUrl: "./menus.component.html",
     styleUrls: ["./menus.component.scss"],
 })
 export class MenusComponent implements OnInit {
+    /**
+     * メニューリスト
+     */
     menus: MenuResponse[] = [];
 
-    constructor() {}
+    constructor(private transactionService: TransactionService, private menuService: MenuService) {}
 
     ngOnInit(): void {
-        // TODO: メニューリスト取得APIから取得
-        this.menus = [
-            {
-                id: 1,
-                shopId: 1,
-                name: "アスパラガスとベーコンのパスタ",
-                price: 1200,
-                imageUrl: "https://gahag.net/img/201512/21s/gahag-0038751717-1.jpg",
-            },
-            {
-                id: 2,
-                shopId: 1,
-                name: "濃厚生クリームのカルボナーラ",
-                price: 1320,
-                imageUrl: "https://gahag.net/img/201512/21s/gahag-0038751717-1.jpg",
-            },
-            {
-                id: 3,
-                shopId: 1,
-                name: "本場の味を厳格に再現 ペペロンチーノ",
-                price: 1150,
-                imageUrl: "https://gahag.net/img/201512/21s/gahag-0038751717-1.jpg",
-            },
-            {
-                id: 4,
-                shopId: 1,
-                name: "アスパラガスとベーコンのパスタ",
-                price: 1200,
-                imageUrl: "https://gahag.net/img/201512/21s/gahag-0038751717-1.jpg",
-            },
-            {
-                id: 5,
-                shopId: 1,
-                name: "濃厚生クリームのカルボナーラ",
-                price: 1320,
-                imageUrl: "https://gahag.net/img/201512/21s/gahag-0038751717-1.jpg",
-            },
-            {
-                id: 6,
-                shopId: 1,
-                name: "本場の味を厳格に再現 ペペロンチーノ",
-                price: 1150,
-                imageUrl: "https://gahag.net/img/201512/21s/gahag-0038751717-1.jpg",
-            },
-            {
-                id: 7,
-                shopId: 1,
-                name: "アスパラガスとベーコンのパスタ",
-                price: 1200,
-                imageUrl: "https://gahag.net/img/201512/21s/gahag-0038751717-1.jpg",
-            },
-            {
-                id: 8,
-                shopId: 1,
-                name: "濃厚生クリームのカルボナーラ",
-                price: 1320,
-                imageUrl: "https://gahag.net/img/201512/21s/gahag-0038751717-1.jpg",
-            },
-            {
-                id: 9,
-                shopId: 1,
-                name: "本場の味を厳格に再現 ペペロンチーノ",
-                price: 1150,
-                imageUrl: "https://gahag.net/img/201512/21s/gahag-0038751717-1.jpg",
-            },
-        ];
+        // メニューリストを取得
+        this.transactionService
+            .getLoginTransaction()
+            .pipe(untilDestroyed(this))
+            .subscribe((response) => {
+                this.menuService
+                    .getMenus({ shop_id: response.shopId })
+                    .pipe(untilDestroyed(this))
+                    .subscribe((response) => {
+                        this.menus = response.menus;
+                    });
+            });
     }
 }
