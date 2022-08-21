b391dae6488fd069a12d2c5f711c19c3d0f0991f
averak <abe12@mccc.jp>
2022-08-21 21:27:13 +0900

    [feat] タブ遷移時にデータを再取得

--- app/src/main/client/src/app/customer/component/order/order.component.ts
+++ app/src/main/client/src/app/customer/component/order/order.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MenusComponent } from "@customer/component/order/menus/menus.component";
+import { CartComponent } from "@customer/component/order/cart/cart.component";
+import { OrderHistoryComponent } from "@customer/component/order/order-history/order-history.component";
 
 @Component({
     selector: "app-order",
@@ -6,7 +9,20 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./order.component.scss"],
 })
 export class OrderComponent implements OnInit {
+    @ViewChild(MenusComponent) menusComponent!: MenusComponent;
+    @ViewChild(CartComponent) cartComponent!: CartComponent;
+    @ViewChild(OrderHistoryComponent) orderHistoryComponent!: OrderHistoryComponent;
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    /**
+     * タブ切り替え時に呼ばれる
+     */
+    switchTab(): void {
+        this.menusComponent.ngOnInit();
+        this.cartComponent.ngOnInit();
+        this.orderHistoryComponent.ngOnInit();
+    }
 }
