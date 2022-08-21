0791e3a414319444ef38fee9dd074e2a8f09f8b5
fkd6280 <theearth6280@outlook.jp>
2022-08-21 18:15:53 +0900

    サイドナビのアイコン-テキスト間に余白を入れた

--- app/src/main/client/src/app/shared/component/header/header.component.ts
+++ app/src/main/client/src/app/shared/component/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs/operators";
 
 @Component({
     selector: "app-header",
@@ -6,7 +8,28 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
-    constructor() {}
+    /**
+     * サイドナビを有効化するかどうか
+     */
+    isSidenavValid: boolean = false;
+
+    constructor(private router: Router) {
+        // ナビゲーション終了時にサイドナビの状態を定義
+        this.router.events
+            .pipe(filter((e) => e instanceof NavigationEnd))
+            .subscribe(() => this.setIsSidenavValid());
+    }
 
     ngOnInit(): void {}
+
+    /**
+     * サイドナビの状態をセットする
+     */
+    setIsSidenavValid(): void {
+        this.isSidenavValid = true;
+        // ホームルートにいる場合、サイドナビを無効化
+        if (this.router.url === "/") {
+            this.isSidenavValid = false;
+        }
+    }
 }
