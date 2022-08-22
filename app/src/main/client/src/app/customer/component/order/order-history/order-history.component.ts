e9c4159c42782566577d07c18bacd82adff29dbb
fkd6280 <theearth6280@outlook.jp>
2022-08-23 04:45:38 +0900

    [feat]履歴画面で頼み終わったメニューを見られるようにした

--- app/src/main/client/src/app/customer/component/order/order-history/order-history.component.ts
+++ app/src/main/client/src/app/customer/component/order/order-history/order-history.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from "@angular/core";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { TransactionService } from "@api/services/transaction.service";
+import { OrderMenuResponse } from "@api/models/order-menu-response";
 
+@UntilDestroy()
 @Component({
     selector: "app-order-history",
     templateUrl: "./order-history.component.html",
     styleUrls: ["./order-history.component.scss"],
 })
 export class OrderHistoryComponent implements OnInit {
-    constructor() {}
+    menus: OrderMenuResponse[] = [];
 
-    ngOnInit(): void {}
+    constructor(private transactionService: TransactionService) {}
+
+    ngOnInit(): void {
+        this.menus = [];
+        // 購入履歴を取得
+        this.transactionService
+            .getLoginTransaction()
+            .pipe(untilDestroyed(this))
+            .subscribe((response) => {
+                response.orders.forEach((order) => {
+                    order.menus.forEach((menu) => {
+                        this.menus.push(menu);
+                    });
+                });
+            });
+    }
 }
