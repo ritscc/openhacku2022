c8de67e496d97ad40badde92abe1e8c89ae0a9e7
averak <abe12@mccc.jp>
2022-08-23 21:49:35 +0900

    [feat] /にアクセスされた時は静的コンテンツを配信

--- app/src/main/java/cc/rits/openhacku2022/OpenHackU2022Application.java
+++ app/src/main/java/cc/rits/openhacku2022/OpenHackU2022Application.java
@@ -2,12 +2,21 @@ package cc.rits.openhacku2022;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class OpenHackU2022Application {
 
     public static void main(String[] args) {
         SpringApplication.run(OpenHackU2022Application.class, args);
+    }
+
+    @RequestMapping("/**/{path:[^\\.]*}")
+    public String redirect(@PathVariable String path) {
+        return "forward:/";
     }
 
 }
