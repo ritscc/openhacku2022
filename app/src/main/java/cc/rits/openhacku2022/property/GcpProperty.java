dd22acd42deb700cade5cd28e11f05891f577e8a
averak <abe12@mccc.jp>
2022-08-25 16:08:14 +0900

    [feat] ローカル環境からGCSにアップロードする

--- app/src/main/java/cc/rits/openhacku2022/property/GcpProperty.java
+++ app/src/main/java/cc/rits/openhacku2022/property/GcpProperty.java
@@ -1,8 +1,9 @@
 package cc.rits.openhacku2022.property;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
 
 /**
  * GCPプロパティ
@@ -16,6 +17,11 @@ public class GcpProperty {
      * プロジェクトID
      */
     String projectId;
+
+    /**
+     * 資格情報のファイルパス
+     */
+    String credentialsPath;
 
     /**
      * Cloud Storage
--- app/src/main/java/cc/rits/openhacku2022/util/FileStorageUtil.java
+++ app/src/main/java/cc/rits/openhacku2022/util/FileStorageUtil.java
@@ -1,10 +1,12 @@
 package cc.rits.openhacku2022.util;
 
+import java.io.FileInputStream;
+
 import org.springframework.stereotype.Component;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.StorageException;
 import com.google.cloud.storage.StorageOptions;
 
 import cc.rits.openhacku2022.exception.ErrorCode;
@@ -33,21 +35,25 @@ public class FileStorageUtil {
      * @return url
      */
     public String upload(final FileModel fileModel) {
-        final var url =
-            String.format("https://storage.googleapis.com/%s/%s", this.gcpProperty.getCloudStorage().getBucketName(), fileModel.getUuid());
+        final var url = String.format("https://storage.googleapis.com/%s/images/%s", //
+            this.gcpProperty.getCloudStorage().getBucketName(), //
+            fileModel.getUuid());
 
         if (!this.gcpProperty.getCloudStorage().isEnabled()) {
             return url;
         }
-
-        final var storage = StorageOptions.newBuilder().setProjectId(gcpProperty.getProjectId()).build().getService();
         try {
+            final var storage = StorageOptions.newBuilder() //
+                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(this.gcpProperty.getCredentialsPath()))) //
+                .setProjectId(gcpProperty.getProjectId()) //
+                .build().getService();
+
             final var blobId = BlobId.of(gcpProperty.getCloudStorage().getBucketName(), IMAGE_PATH + fileModel.getUuid());
             final var blobInfo = BlobInfo.newBuilder(blobId).build();
             storage.create(blobInfo, fileModel.getContent());
 
             return url;
-        } catch (final StorageException e) {
+        } catch (final Exception e) {
             log.error(e.getMessage(), e);
             throw new InternalServerErrorException(ErrorCode.FAILED_TO_UPLOAD_FILE);
         }
