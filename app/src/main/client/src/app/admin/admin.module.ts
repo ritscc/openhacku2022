0cc7667fd36c6ba3a892160626d742b1418ed48f
averak <abe12@mccc.jp>
2022-08-25 15:48:59 +0900

    [feat] メニュー登録画面を作成

--- app/src/main/client/src/app/admin/admin.module.ts
+++ app/src/main/client/src/app/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "@shared/shared.module";
 import { LoginComponent } from "./component/login/login.component";
 import { MenusComponent } from "./component/menus/menus.component";
+import { MenuCreateFormComponent } from "./component/menu-create-form/menu-create-form.component";
 
 @NgModule({
-    declarations: [LoginComponent, MenusComponent],
+    declarations: [LoginComponent, MenusComponent, MenuCreateFormComponent],
     imports: [SharedModule],
 })
 export class AdminModule {}
