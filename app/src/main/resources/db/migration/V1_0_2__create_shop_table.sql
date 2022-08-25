6729735351bd0c2c8a22ab6ea21754b33e61a025
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2022-08-26 00:44:12 +0900

    [feature] shop_idとnumberの複合ユニーク制約を追加

--- app/src/main/resources/db/migration/V1_0_2__create_shop_table.sql
+++ app/src/main/resources/db/migration/V1_0_2__create_shop_table.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS `shop_table`
     `created_at` DATETIME     NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME     NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
+    UNIQUE INDEX `shop_id_and_number_UNIQUE` (`shop_id` ASC, `number` ASC) VISIBLE,
     INDEX `fk_shop_table_shop_id_idx` (`shop_id` ASC) VISIBLE,
     CONSTRAINT `fk_shop_table_shop_id`
         FOREIGN KEY (`shop_id`)
